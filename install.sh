e74b397abe12d057c570b3c74034067464251e89
Yilin Wang <yilin@LittleMac.local>
2020-05-27 16:51:19 +0800

    install

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
-cp vimrc ~/.vimrc
-cp bash_profile ~/.bash_profile
 cp tmux.conf ~/.tmux.conf
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sh install_vim.sh
---
+++ install_vim.sh
@@ -0,0 +1,3 @@
+cp -r vim ~/.vim
+cp vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
